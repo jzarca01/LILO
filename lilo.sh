ef2ebf2c59f3531cad7838aa6801f0f88900c7db
Jérémie Zarca <jzarca01@users.noreply.github.com>
2021-05-10 12:18:25 +0200

    Update lilo.sh

--- lilo.sh
+++ lilo.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-sudo service bluetooth stop
-sudo hciconfig hci0 up
+UNAME=$(uname)
+if [ "$UNAME" == "Linux" ] ; then
+    sudo service bluetooth stop
+    sudo hciconfig hci0 up
+fi
 
 DAYOFWEEK=$(date +"%u")
 echo DAYOFWEEK: $DAYOFWEEK
 
-if [ "${DAYOFWEEK}" -eq 5 ]
+if [ "${DAYOFWEEK}" -le 5 ]
 then
-    ./LILO -t 09,00,23,59
+    ./LILO -t 10,00,22,15
 
 elif [ "${DAYOFWEEK}" -eq 6 ]
 then
-    ./LILO -t 12,00,23,59
+    ./LILO -t 12,00,23,00
 
 elif [ "${DAYOFWEEK}" -eq 7 ]
 then
     ./LILO -t 12,00,23,00
-
-else
-    ./LILO
 fi
